b6d98a9f1559828b1e0b5af9c5792038d2476b46
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 23:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 let magazine = ["h", "e", "r", "e", "a", "r", "e", "s", "o", "m",
 "e", "n", "i", "c", "e", "c", "l", "o", "t", "h", "e", "s", "t"]
 function buildHistogram(mag) {
-  // define a new histogram
-  // loop through the chars in the magazine
-    // add/update the node in the historgram
-
-
   var arr = {}
-  for (let i = 0; i < mag.length; i++) {
-    arr[mag[i]] == null ? arr[mag[i]] = 1 : arr[mag[i]] = arr[mag[i]] + 1;
-  }
-
-  console.log(arr)
+  mag.forEach(e => arr[e] == null ? arr[e] = 1 : arr[e] = arr[e] + 1;)
   return arr
 }
 
