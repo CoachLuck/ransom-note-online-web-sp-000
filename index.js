d8c8a1db0cd3cd99ae7a8d323c3d78a5856e0eaf
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 23:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let magazine = ["h", "e", "r", "e", "a", "r", "e", "s", "o", "m",
 "e", "n", "i", "c", "e", "c", "l", "o", "t", "h", "e", "s", "t"]
+let note = "hereisat"
+
 function buildHistogram(mag) {
   var arr = {}
   mag.forEach(e => arr[e] == null ? arr[e] = 1 : arr[e] = arr[e] + 1)
@@ -13,4 +15,4 @@ function canBuildNote(mag, note) {
   console.log(magGram)
 }
 
-console.log(buildHistogram(magazine))
+canBuildNote(magazine, note)
