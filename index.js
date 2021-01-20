3e9ff8e7003a883ada0fbfd75dc37c6aae7c8b80
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 23:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function buildHistogram(mag) {
     // add/update the node in the historgram
 
 
-  var arr = { `${mag[0]}: 1 }
+  var arr = { `${mag[0]}`: 1 }
   console.log(arr)
   for (let i = 0; i < mag.length; i++) {
     let current = mag[i]
