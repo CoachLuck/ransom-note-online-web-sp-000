b91f0382dffc5c15401b308143874df40ba7b8e4
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 23:00:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+let magazine = ["h", "e", "r", "e", "a", "r", "e", "s", "o", "m",
+"e", "n", "i", "c", "e", "c", "l", "o", "t", "h", "e", "s", "t"]
+function buildHistogram(mag) {
+  // define a new histogram
+  // loop through the chars in the magazine
+    // add/update the node in the historgram
+
+  let hist = {[msg[0], 1]}
+  for (let i = 0; i < mag.length; i++) {
+    let current = msg[i]
+  }
+}
+
+function canBuildNote() {
+
+}
